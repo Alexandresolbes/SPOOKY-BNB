6171a3bfb0f2949c33f1519ae8f98eb73bed5086
Isabel Lamim <lamim@protonmail.com>
2022-08-23 17:40:14 +0200

    added dependencies >> destroy

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :listing
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :status, inclusion: { in: %w(pending active cancelled past) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :listings
-  has_many :bookings
+  has_many :listings, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
   has_one_attached :photo
   validates :photo, presence: true
