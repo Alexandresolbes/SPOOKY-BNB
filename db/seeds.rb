d1904a0c76bc7a9df180d7a89e860ddf295ce3e4
Laura Latuillerie <laura.latuillerie@gmail.com>
2022-08-24 12:22:34 +0200

    Add precise addresses to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,7 +68,7 @@ file_2 = URI.open("https://a0.muscache.com/im/pictures/8fa09ffe-3e8d-43ac-8e00-c
 dracula = Listing.new(
   title: "Dracula's Mansion",
   description: "Discover Dracula's Home",
-  address: "1 Vampire's Street, Transylvania",
+  address: "Str. G-ral Traian Mosoiu, nr.24, Bran",
   max_guests: rand(1..10),
   price_per_night: rand(20...500),
   user_id: User.all.sample.id
@@ -83,7 +83,7 @@ file_2 = URI.open("https://static01.nyt.com/images/2012/10/24/blogs/20121024-len
 aokigahara = Listing.new(
   title: "Aokigahara Forest",
   description: "Set up your tent and meet yûreis in the most famous forest in Japan",
-  address: "At the feet of mount Fuji, Japan",
+  address: "Narusawa, Fujikawaguchiko, Minamitsuru District, Yamanashi 401-0300, Japan",
   max_guests: rand(1..10),
   price_per_night: rand(20...500),
   user_id: User.all.sample.id
@@ -97,7 +97,7 @@ file_2 = URI.open("https://res.cloudinary.com/laura-latuillerie/image/upload/v16
 sanatorium = Listing.new(
   title: "Sanatorium Simone Weber",
   description: "Visit the oldest haunted hospital in France",
-  address: "Lay-St-Christophe, France",
+  address: "4 rue Professeur Montaut, 54690 Lay Saint Christophe",
   max_guests: rand(1..10),
   price_per_night: rand(20...500),
   user_id: User.all.sample.id
@@ -111,7 +111,7 @@ file_2 = URI.open("https://res.cloudinary.com/laura-latuillerie/image/upload/v16
 bunker = Listing.new(
   title: "Old Bunker of Schönberger Straße",
   description: "An old bunker which is now haunted by Nazis",
-  address: "3 Schönberger Straße, Germany",
+  address: "Schöneberger Str. 23A, 10963 Berlin, Germany",
   max_guests: rand(1..10),
   price_per_night: rand(20...500),
   user_id: User.all.sample.id
@@ -125,7 +125,7 @@ file_2 = URI.open("https://res.cloudinary.com/laura-latuillerie/image/upload/v16
 saloon = Listing.create(
   title: "Red Onion Saloon",
   description: "Come and visit the former women who used to work there",
-  address: "Klondike Alaska, USA",
+  address: "201 Broadway, Skagway, AK 99840, United States",
   max_guests: rand(1..10),
   price_per_night: rand(20...500),
   user_id: User.all.sample.id
@@ -139,7 +139,7 @@ file_2 = URI.open("https://static2.tripoto.com/media/filter/tst/img/295383/TripD
 fort = Listing.new(
   title: "Bhangarh's Fort",
   description: "Sleep and meet a Tantrik Black Mage",
-  address: "Rajasthan, India",
+  address: "Tehsil, Gola ka baas, Rajgarh, Bhangarh, Rajasthan 301410, India",
   max_guests: rand(1..10),
   price_per_night: rand(20...500),
   user_id: User.all.sample.id
@@ -167,7 +167,7 @@ file_2 = URI.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSWBcMV
 church = Listing.new(
   title: "Egg Hill Church",
   description: "Enjoy your stay at one of the most haunted church of the world, beware of the pastor....",
-  address: "Spring Hills Pennsylvania, USA",
+  address: "289 T407, Spring Mills, PA 16875, United States",
   max_guests: rand(1..10),
   price_per_night: rand(20...500),
   user_id: User.all.sample.id
