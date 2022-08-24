efffb050306b91cb904b7f47bffc617f897846be
Ait Medjeber Lounes <monsieur.aitmejber@gmail.com>
2022-08-24 10:45:52 +0200

    Geocoder addcolumn et modif du model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,6 @@ class Listing < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
   has_many_attached :photos
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
