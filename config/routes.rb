408c724575094a1b0d724d3763ca57560bab9bcb
Laura Latuillerie <laura.latuillerie@gmail.com>
2022-08-25 12:22:22 +0200

    Create route, method in controller pages and view for profile user

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
 
   get "/bookings", to: "pages#bookings_as_guest"
   get "/bookings_host", to: "pages#bookings_as_host"
+  get "/profile", to: "pages#profile"
 end
